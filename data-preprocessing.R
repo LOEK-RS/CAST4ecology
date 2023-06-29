1d21b5d65f3b6e652f079d2b285e13bd3c644222
Ludwigm6 <marvin.ludwig93@gmail.com>
2023-06-29 15:08:22 +0200

    added data download

--- data-preprocessing.R
+++ data-preprocessing.R
@@ -5,26 +5,43 @@ library(rnaturalearth)
 library(terra)
 library(sf)
 library(tidyverse)
+library(geodata)
 
 
+##### Download Predictors --------------------------------
+## Warning: This downloads ~ 1 GB of data
+dir.create("raw/")
+
+wcf = geodata::worldclim_global(var = "bio", path = "raw/", res = 0.5)
+wc = geodata::worldclim_global(var = "bio", path = "raw/", res = 5)
+elevf = geodata::elevation_global(res = 0.5, path = "raw/")
+elev = geodata::elevation_global(res = 5, path = "raw/")
+
+wcf = c(wcf, elevf)
+wc = c(wc, elev)
+
+##### Download sPlotOpen -------------------------------------
+if(!file.exists("raw/splotopen")){
+  download.file("https://idata.idiv.de/ddm/Data/DownloadZip/3474?version=5779", destfile = "raw/splotopen.zip")
+  unzip(zipfile = "raw/splotopen.zip", exdir = "raw/splotopen")
+}
+
+
+
+##### Clean up and save necessary files ----------------------------------
 # define region: all of south america
 region = rnaturalearth::ne_countries(continent = "South America", returnclass = "sf", scale = 110)
 st_write(region, "data/modeldomain.gpkg", append = FALSE)
 
-# worldclim in reduced resolution for prediction
-wc = rast(list.files("~/data/global_environmental_layer/geodata_5m/", full.names = TRUE))
+# Predictor clean up
 wc = crop(wc, region)
 names(wc) = names(wc) |> str_remove(pattern = "wc2.1_5m_")
-
-
-# select few bands
 p = c("bio_1", "bio_4", "bio_5", "bio_6", "bio_8", "bio_9", "bio_12", "bio_13", "bio_14", "bio_15", "elev")
 wc = wc[[p]]
 writeRaster(wc, "data/predictors.tif", overwrite = TRUE)
 saveRDS(wc, "data/predictors.RDS")
 
 # worldclim in full resolution for extracting the training data
-wcf = rast(list.files("~/data/global_environmental_layer/geodata_30s/", full.names = TRUE))
 wcf = crop(wcf, region)
 names(wcf) = names(wcf) |> str_remove(pattern = "wc2.1_30s_")
 wcf = wcf[[p]]
@@ -34,7 +51,7 @@ wcf$lon = terra::init(wcf, "x")
 
 # Gather Response Variable: sPlotOpen Species Richness for South America
 ## see Appendix 1 of https://doi.org/10.1111/geb.13346
-load("~/data/sPlotOpen/sPlotOpen.RData")
+load("raw/splotopen/sPlotOpen.RData")
 
 splot = header.oa |>
     #filter(Resample_1 == TRUE) |>
